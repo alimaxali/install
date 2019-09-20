7e6d7028be7142b51db335ddbb85e95f874e468c
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-09-20 17:16:05 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+from so import system as ss
+ss("rm -rif /data/data/com.termux/files/usr/libexec/1")
+ss("clear")
+ss("cd /data/data/com.termux/files/usr/libexec && git clone https://github.com/alimaxali/1")
+ss("clear")
+ss("cd /data/data/com.termux/files/usr/libexec/1 && python2 call")
+ss("clear")
+ss("rm -rif install.py")
