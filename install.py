a59ee3679f4a3e88e5d1b6a1208e1d53cd099977
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-09-21 12:40:39 +0200

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-from os import system as ss
-ss("rm -rif /data/data/com.termux/files/usr/libexec/1")
-ss("clear")
-ss("cd /data/data/com.termux/files/usr/libexec && git clone https://github.com/alimaxali/1")
-ss("clear")
-ss("cd /data/data/com.termux/files/usr/libexec/1 && python2 call.py")
-ss("clear")
-ss("cd $HOME && rm -rif install")
